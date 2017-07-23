0b50ef96e747d74ae6380582741ef744b482e34a
Tristan Larsin <tlarsin@gmail.com>
2017-07-22 19:46:26 -0500

    Updated directory structure a bit. I also removed my api key for the mailing form.

--- assets/script/send.php
+++ assets/script/send.php
@@ -1,9 +1,9 @@
 <?php
 if( isset($_POST) ){
-	
+
 	$postData = $_POST;
 	$mailgun = sendMailgun($postData);
-	
+
 	if($mailgun) {
 	echo "Great success.";
   } else {
@@ -12,37 +12,37 @@ if( isset($_POST) ){
 }
 
 function sendMailgun($data) {
- 
-  $api_key = 'key-20fd2636b55aacabaca702cdc89d995e';
+
+  $api_key = '';
   $api_domain = 'tristanlarsin.com';
   $send_to = 'tlarsin@gmail.com';
-	
+
 	// sumbission data
 		$ipaddress = $_SERVER['REMOTE_ADDR'];
 		$date = date('d/m/Y');
 		$time = date('H:i:s');
-	
+
 	// form data
 		$postcontent = $data['data'];
-		$reply = $data['senderAddress'];	
- 
+		$reply = $data['senderAddress'];
+
   $messageBody = "<p>You have received a new message from the contact form on your website.</p>
 				{$postcontent}
 				<p>This message was sent from the IP Address: {$ipaddress} on {$date} at {$time}</p>";
- 
+
   $config = array();
   $config['api_key'] = $api_key;
   $config['api_url'] = 'https://api.mailgun.net/v2/'.$api_domain.'/messages';
- 
+
   $message = array();
   $message['from'] = $reply;
   $message['to'] = $send_to;
   $message['h:Reply-To'] = $reply;
   $message['subject'] = "New Message from your Mailgun Contact Form";
   $message['html'] = $messageBody;
- 
+
   $curl = curl_init();
- 
+
   curl_setopt($curl, CURLOPT_URL, $config['api_url']);
   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
   curl_setopt($curl, CURLOPT_USERPWD, "api:{$config['api_key']}");
@@ -50,14 +50,14 @@ function sendMailgun($data) {
   curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
   curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-  curl_setopt($curl, CURLOPT_POST, true); 
+  curl_setopt($curl, CURLOPT_POST, true);
   curl_setopt($curl, CURLOPT_POSTFIELDS,$message);
- 
+
   $result = curl_exec($curl);
- 
+
   curl_close($curl);
   return $result;
- 
+
 }
 
 

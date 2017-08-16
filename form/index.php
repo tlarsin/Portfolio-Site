5cda87270d71dcb2a301f93e8a34079ab4107f11
Tristan Larsin <tlarsin@gmail.com>
2017-08-15 20:06:50 -0500

    Added Google recaptcha to keep from spamming spiders.

--- form/index.php
+++ form/index.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(E_ALL ^ E_NOTICE);
 /*
 Using rwbaker's repo. Link: https://github.com/rwbaker/PHP-Email-Form
@@ -38,60 +39,144 @@ $continue = "/";
 Step 3: Save this file (/thankyou/index.php) and upload it together with your webpage containing the form to your webspace.  IMPORTANT - The file name is case sensitive!  You must save it exactly as it is named above!
 */
 $errors = array();
+
+
+
+// Google recaptcha
+$response="";
+if(isset($_REQUEST["g-recaptcha-response"])){
+
+	$response=$_REQUEST["g-recaptcha-response"];
+
+}
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "secret=6LeDfywUAAAAAB0_VKg2pd-sudAeQELDSJBiDr0x&response=".$response);
+
+// in real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS,
+//          http_build_query(array('postvar1' => 'value1')));
+
+// receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+$server_output=json_decode($server_output,1);
+if($server_output["success"]) {
+	echo "";
+}
+else {
+	$errors[] = "Captcha Is not Valid";
+}
+// End of Google recaptcha test
+
+// Start of mailing code
+
 // Remove $_COOKIE elements from $_REQUEST.
-if(count($_COOKIE)){foreach(array_keys($_COOKIE) as $value){unset($_REQUEST[$value]);}}
+if(count($_COOKIE)) {
+	foreach(array_keys($_COOKIE) as $value){unset($_REQUEST[$value]);}
+}
 // Validate email field.
-if(isset($_REQUEST['email']) && !empty($_REQUEST['email']))
-{
-$_REQUEST['email'] = trim($_REQUEST['email']);
-if(substr_count($_REQUEST['email'],"@") != 1 || stristr($_REQUEST['email']," ")){$errors[] = "Email address is invalid";}else{$exploded_email = explode("@",$_REQUEST['email']);if(empty($exploded_email[0]) || strlen($exploded_email[0]) > 64 || empty($exploded_email[1])){$errors[] = "Email address is invalid";}else{if(substr_count($exploded_email[1],".") == 0){$errors[] = "Email address is invalid";}else{$exploded_domain = explode(".",$exploded_email[1]);if(in_array("",$exploded_domain)){$errors[] = "Email address is invalid";}else{foreach($exploded_domain as $value){if(strlen($value) > 63 || !preg_match('/^[a-z0-9-]+$/i',$value)){$errors[] = "Email address is invalid"; break;}}}}}}
+if(isset($_REQUEST['email']) && !empty($_REQUEST['email'])) {
+	$_REQUEST['email'] = trim($_REQUEST['email']);
+	if(substr_count($_REQUEST['email'],"@") != 1 || stristr($_REQUEST['email']," ")) {
+		$errors[] = "Email address is invalid";
+	}
+	else {
+		$exploded_email = explode("@",$_REQUEST['email']);if(empty($exploded_email[0]) || strlen($exploded_email[0]) > 64 || empty($exploded_email[1])){$errors[] = "Email address is invalid";}else{if(substr_count($exploded_email[1],".") == 0){$errors[] = "Email address is invalid";}else{$exploded_domain = explode(".",$exploded_email[1]);if(in_array("",$exploded_domain)){$errors[] = "Email address is invalid";}else{foreach($exploded_domain as $value){if(strlen($value) > 63 || !preg_match('/^[a-z0-9-]+$/i',$value)){$errors[] = "Email address is invalid"; break;}}}}}
+	}
 }
 // Check referrer is from same site.
-if(!(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']) && stristr($_SERVER['HTTP_REFERER'],$_SERVER['HTTP_HOST']))){$errors[] = "You must enable referrer logging to use the form";}
+if(!(isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']) && stristr($_SERVER['HTTP_REFERER'],$_SERVER['HTTP_HOST']))) {
+	$errors[] = "You must enable referrer logging to use the form";
+}
 // Check for a blank form.
-function recursive_array_check_blank($element_value)
-{
-global $set;
-if(!is_array($element_value)){if(!empty($element_value)){$set = 1;}}
-else
-{
-foreach($element_value as $value){if($set){break;} recursive_array_check_blank($value);}
+function recursive_array_check_blank($element_value) {
+	global $set;
+	if(!is_array($element_value)) {
+		if(!empty($element_value)) {
+			$set = 1;
+		}
+	}
+	else {
+		foreach($element_value as $value) {
+			if($set) {
+				break;
+			}
+			recursive_array_check_blank($value);
+		}
+	}
 }
-}
+
 recursive_array_check_blank($_REQUEST);
-if(!$set){$errors[] = "You cannot send a blank form";}
+if(!$set) {
+	$errors[] = "You cannot send a blank form";
+}
 unset($set);
 // Display any errors and exit if errors exist.
-if(count($errors)){foreach($errors as $value){print "$value<br>";} exit;}
-if(!defined("PHP_EOL")){define("PHP_EOL", strtoupper(substr(PHP_OS,0,3) == "WIN") ? "\r\n" : "\n");}
+if(count($errors)) {
+	foreach($errors as $value) {
+		print "$value<br>";
+	}
+	exit;
+}
+if(!defined("PHP_EOL")) {
+	define("PHP_EOL", strtoupper(substr(PHP_OS,0,3) == "WIN") ? "\r\n" : "\n");
+}
 // Build message.
-function build_message($request_input){if(!isset($message_output)){$message_output ="";}if(!is_array($request_input)){$message_output = $request_input;}else{foreach($request_input as $key => $value){if(!empty($value)){if(!is_numeric($key)){$message_output .= str_replace("_"," ",ucfirst($key)).": ".build_message($value).PHP_EOL.PHP_EOL;}else{$message_output .= build_message($value).", ";}}}}return rtrim($message_output,", ");}
+function build_message($request_input) {
+	if(!isset($message_output)) {
+		$message_output ="";
+	}
+	if(!is_array($request_input)) {
+		$message_output = $request_input;
+	}
+	else {
+		foreach($request_input as $key => $value) {
+			if(!empty($value)) {
+				if(!is_numeric($key)) {
+					$message_output .= str_replace("_"," ",ucfirst($key)).": ".build_message($value).PHP_EOL.PHP_EOL;
+				}
+				else {
+					$message_output .= build_message($value).", ";
+				}
+			}
+		}
+	}
+
+	return rtrim($message_output,", ");
+}
 $message = build_message($_REQUEST);
 $message = $message . PHP_EOL.PHP_EOL."-- ".PHP_EOL."Thank you for using FormToEmail from http://FormToEmail.com";
 $message = stripslashes($message);
 $subject = stripslashes($subject);
-if($from_email)
-{
-$headers = "From: " . $from_email;
-$headers .= PHP_EOL;
-$headers .= "Reply-To: " . $_REQUEST['email'];
+if($from_email) {
+	$headers = "From: " . $from_email;
+	$headers .= PHP_EOL;
+	$headers .= "Reply-To: " . $_REQUEST['email'];
 }
-else
-{
-$from_name = "";
-if(isset($_REQUEST['name']) && !empty($_REQUEST['name'])){$from_name = stripslashes($_REQUEST['name']);}
-$headers = "From: {$from_name} <{$_REQUEST['email']}>";
+else {
+	$from_name = "";
+	if(isset($_REQUEST['name']) && !empty($_REQUEST['name'])) {
+		$from_name = stripslashes($_REQUEST['name']);
+	}
+	$headers = "From: {$from_name} <{$_REQUEST['email']}>";
 }
 mail($my_email,$subject,$message,$headers);
-/*
-<b>Thank you <?php if(isset($_REQUEST['name'])){print stripslashes($_REQUEST['name']);} ?></b>
-*/
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="refresh" content="5;url=<?php print $site_url; ?>">
 	<title>Thank you for contacting <?php print $site_name; ?></title>
 	<style>
@@ -108,7 +193,7 @@ mail($my_email,$subject,$message,$headers);
 </head>
 <body>
 	<div id="thankyou">
-		<p><strong>Thank you for contacting us, your message has been sent.</strong></p>
+		<p><strong>Thank you for contacting me, your message has been sent.</strong></p>
 		<p>If you're not redirected in 5 seconds, then <a href="<?php print $continue; ?>">click here to go to the homepage.</a></p>
 	</div>
 </body>

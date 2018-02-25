31b4161be4253440c892f8006785a18d848604fb
Tristan Larsin <tlarsin@gmail.com>
2018-02-25 13:06:52 -0600

    Working on designing my portfolio website in ReactJS.

---
+++ my-portfolio/src/js/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import '../css/Header.css'
+
+class Header extends Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+    return(
+      <div className='top'>
+        <h1>Tristan Larsin</h1>
+
+        <Link to="/" className='child'>Home</Link>
+        <Link to="/" className='child'>Projects</Link>
+
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ my-portfolio/src/routes/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import Header from '../js/Header.js';
+
+export default() => (
+  <div>
+    <Header/>
+  </div>
+);

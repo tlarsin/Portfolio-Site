afde938c72a14afa98d4ba71bbda3b113b9a14b3
Tristan Larsin <tlarsin@gmail.com>
2018-11-11 11:58:10 -0600

    Redesigned website and updated content.

--- my-portfolio/src/App.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import Routes from './routes';
-
-export default () => <Routes />;
--- my-portfolio/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Router, Route, hashHistory } from 'react-router'
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
